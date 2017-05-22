f539e46a2a5d45be6102de91ff0ae993e8664b58
Seth Baughman <sbaughman1@me.com>
2017-05-22 12:09:54 -0400

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './Header'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="expanded row header">
-          <div className="clearfix">
-            <div className="float-left logo">
-              <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
-            </div>
-            <div className="float-right nav-links">
-              <a href="#">Home</a>
-              <a href="#">About</a>
-              <a href="#">Log in</a>
-            </div>
-          </div>
-          <div className="headline text-center small-12 columns">
-            <h1>The Dwarf Underground</h1>
-            <h3>Delving deep for the <span className="gold">shiniest</span> news</h3>
-          </div>
-        </div>
+        <Header />
 
         <main className="expanded row">
           <div className="large-8 medium-12 columns article">
---
+++ src/Header.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import './Header.css'
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="expanded row header">
+        <div className="clearfix">
+          <div className="float-left logo">
+            <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
+          </div>
+          <div className="float-right nav-links">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Log in</a>
+          </div>
+        </div>
+        <div className="headline text-center small-12 columns">
+          <h1>The Dwarf Underground</h1>
+          <h3>Delving deep for the <span className="gold">shiniest</span> news</h3>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Header
